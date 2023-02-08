8a38a7b44d204d4d6ec4c8545d62af0ad00de88d
Dominik Schmidt <dev@dominik-schmidt.de>
2023-02-08 16:00:11 +0100

    Make assets available again

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from '@ownclouders/extension-vite-config'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   server: {
     port: 9222
-  }
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [{ src: 'node_modules/leaflet-gpx/pin-*.png', dest: 'assets' }]
+    })
+  ]
 })
